492fa5b43b34b8bdecb00127a1c9634406be416d
dverma321 <151020798+dverma321@users.noreply.github.com>
2023-12-28 11:21:08 +0530

    Update authenciate.js

--- middleware/authenciate.js
+++ middleware/authenciate.js
@@ -1,20 +1,15 @@
-const jwt = require('jsonwebtoken')
-const User = require('../model/userSchema')
+const jwt = require('jsonwebtoken');
+const User = require('../model/userSchema');
 
-const Authenciate = async (req, res, next) => {
-
-    try{
-
+const authenciate = async (req, res, next) => {
+    try {
         const token = req.cookies.jwtoken;
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
 
-        const rootUser = await User.findOne({
-            _id:verifyToken._id, "tokens.token":token
-        });
+        const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
 
-        if(!rootUser)
-        {
-            throw new Error('User Not Found...')
+        if (!rootUser) {
+            throw new Error('User not found');
         }
 
         req.token = token;
@@ -22,14 +17,9 @@ const Authenciate = async (req, res, next) => {
         req.userID = rootUser._id;
 
         next();
-
+    } catch (err) {
+        res.status(401).send({ error: 'Unauthorized' });
     }
-    catch(err) {
-        res.status(401).send('Unauthorized: No Token Provided');
-        console.log(err);
+};
 
-    }
-
-}
-
-module.exports = Authenciate
+module.exports = authenciate;
